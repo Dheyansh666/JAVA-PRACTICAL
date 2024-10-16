494910bdf513a7aeb8441ec2945c3a82d7ab3bea
Dheyansh666 <154401642+Dheyansh666@users.noreply.github.com>
2024-10-16 20:28:22 +0530

    Delete Part1 directory

--- Part1/Prac2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Prac2
-{
-    public static void main(String args[]){
-        int current_balance=20;
-    System.out.println("Current Balance= $"+ current_balance);
-    }
-    
-}
